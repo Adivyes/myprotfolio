8eb8eb4b0f6f49e2b14455bd2a0275509aa1dd08
Adivyes <Adivyes@gmail.com>
2020-11-25 15:57:18 +0200

    init

---
+++ test.js
@@ -0,0 +1,75 @@
+// var caseHero = Math.floor(Math.random()*10);
+// switch (caseHero){
+// case 1:
+//         divMain.innerHTML = "sending spiderman" + caseHero;
+// case 2:
+//         divMain.innerHTML = "sending ironman"+ caseHero;
+// case 3:
+//         divMain.innerHTML = "sending hulk"+ caseHero;
+// case 4:
+//         divMain.innerHTML = "sending black panter"+ caseHero;
+// case 5:
+//         divMain.innerHTML = "sending Captain America "+ caseHero;
+// case 6:
+//         divMain.innerHTML = "sending thor"+ caseHero;
+// case 7:
+//         divMain.innerHTML = "sending Ant-Man"+ caseHero;
+// case 8:
+//         divMain.innerHTML = "sending Human Torch"+ caseHero;
+// case 9:
+//         divMain.innerHTML = "sending Mister Fantastic"+ caseHero;
+//         break;
+//         default:
+//             divMain.innerHTML = "n/v"+ caseHero;
+// }
+
+
+
+
+
+var divMain = document.getElementById('mainDiv');
+
+function grade() {
+   let whatgrade = Number(input.value)//
+    // var whatgrade = Math.floor(Math.random()*20);
+switch (whatgrade){
+
+    case 6:
+        divMain.innerHTML = 'שלום כיתה א׳'
+        break;
+    case 7:
+        divMain.innerHTML = 'שלום כיתה ב׳'
+        break;
+    case 8:
+        divMain.innerHTML = 'שלום כיתה ג׳'
+        break;
+    case 9:
+        divMain.innerHTML = 'שלום כיתה ד׳'
+        break;
+    case 10:
+        divMain.innerHTML = 'שלום כיתה ה׳'
+        break;
+    case 11:
+        divMain.innerHTML = 'שלום כיתה ו׳'
+        break;
+    case 12:
+        divMain.innerHTML = 'שלום כיתה ז׳'
+        break;
+    case 13:
+        divMain.innerHTML = 'שלום כיתה ח׳'
+        break;
+    case 14:
+        divMain.innerHTML = 'שלום כיתה ט׳'
+    case 15:
+        divMain.innerHTML = 'שלום כיתה י׳'
+        break;
+    case 16:
+        divMain.innerHTML = 'שלום כיתה יא׳'
+        break;
+    case 17:
+        divMain.innerHTML = 'שלום כיתה יב׳'
+        break;
+        default:
+            divMain.innerHTML = 'n/v'
+}
+}
