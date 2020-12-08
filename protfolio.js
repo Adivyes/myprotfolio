53e00bcc28af079fe2bbeb5fcb98075f6c58eab9
Adivyes <Adivyes@gmail.com>
2020-12-08 17:48:06 +0200

    init

--- protfolio.js
+++ protfolio.js
@@ -1,8 +1,23 @@
 var menubtn = document.getElementById("menubtn")
 var menubtn = document.getElementById("sidenav")
 var menubtn = document.getElementById("menu")
+var timeDate = document.getElementById("dateTime")
 
+ 
 sidenav.style.right = "-250px";
+
+function printTime() {
+    let showtime = new Date();
+    var year = showtime.getFullYear();
+    var month = showtime.getMonth();
+    var day = showtime.getDate();
+    var hours = showtime.getHours();
+    var mins = showtime.getMinutes();
+    var secs = showtime.getSeconds();
+  
+    timeDate.innerHTML = day+'/'+ month +'/'+year+' <br> '+ hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
 
 menubtn.onclick = function(){
     if(sidenav.style.right == "-250px"){
@@ -19,3 +34,4 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 speed: 1000,
 speedAsDuration: true
 });
+
